241c1dec1416a3fdb3af96b44aa99c1f7e587acb
EddieMichael1983 <46540863+EddieMichael1983@users.noreply.github.com>
2019-03-21 21:04:42 -0700

    Update Rock_Paper_Scissors.py

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,43 +1,38 @@
-user_choice = input('Choose rock, paper, or scissors: ') #the computer asks the user for their choice
-
 
 import random
 
 rps = ['rock', 'paper', 'scissors']  #defining random values for the computer to choose from
 
+user_choice2 = 'y'  #this sets the program up to run again later when the user is asked if they want to play again
 
-choice = random.choice(rps) #the computer randomly chooses rock, paper, or scissors
-print(choice)
-
-if user_choice == 'rock' and choice == 'rock':
-    print('tie')
-elif user_choice == 'rock' and choice == 'paper':
-    print('paper covers rock, you lose')
-elif user_choice == 'rock'and choice == 'scissors':
-    print('rock smashes scissors, you win')
-elif user_choice == 'paper' and choice == 'rock':
-    print('paper covers rock, you win')
-elif user_choice == 'paper' and choice == 'paper':
-    print('tie')
-elif user_choice == 'paper' and choice == 'scissors':
-    print('scissors cuts paper, you lose')
-elif user_choice == 'scissors' and choice == 'rock':
-    print('rock smashes scissors, you lose')
-elif user_choice == 'scissors' and choice == 'paper':
-    print('scissors cuts paper, you win')
-elif user_choice == 'scissors' and user_choice == 'scissors':
-    print('tie')     #determines who won and tells the user
-
-user_choice2 = input('Do you want to play again?: y/n ')
-
-while user_choice2 == 'y':
+while user_choice2 == 'y':  #while user_choice2 == y is TRUE the program keeps going
 
     user_choice = input('Choose rock, paper, or scissors: ') #the computer asks the user for their choice
 
+    choice = random.choice(rps) #the computer randomly chooses rock, paper, or scissors
+    print(choice)
 
-    import random
+    if user_choice == 'rock' and choice == 'rock':
+        print('tie')
+    elif user_choice == 'rock' and choice == 'paper':
+        print('paper covers rock, you lose')
+    elif user_choice == 'rock'and choice == 'scissors':
+        print('rock smashes scissors, you win')
+    elif user_choice == 'paper' and choice == 'rock':
+        print('paper covers rock, you win')
+    elif user_choice == 'paper' and choice == 'paper':
+        print('tie')
+    elif user_choice == 'paper' and choice == 'scissors':
+        print('scissors cuts paper, you lose')
+    elif user_choice == 'scissors' and choice == 'rock':
+        print('rock smashes scissors, you lose')
+    elif user_choice == 'scissors' and choice == 'paper':
+        print('scissors cuts paper, you win')
+    elif user_choice == 'scissors' and user_choice == 'scissors':
+        print('tie')     #determines who won and tells the user
 
-    rps = ['rock', 'paper', 'scissors']  #defining random values for the computer to choose from
+
+    user_choice2 = input('Do you want to play again?: y/n ')
 
 
     choice = random.choice(rps) #the computer randomly chooses rock, paper, or scissors
